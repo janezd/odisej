988678f0b206473eb3859b562aed0a60c4e258a5
janezd <janez.demsar@fri.uni-lj.si>
2018-07-05 15:31:41 +0200

    Add game state editing

--- src/game.js
+++ src/game.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Panel, Button, Media, Modal } from 'react-bootstrap'
+import { Panel, Button, Media, Modal, Label, FormControl, ControlLabel, DropdownButton, MenuItem } from 'react-bootstrap'
 import blocks from "./createBlocks"
 import { locations, items, flags, variables, allLocations, restoreLocally, storeLocally, packBlockArgs } from './quill'
 
@@ -9,7 +9,69 @@ const ITEM_DOES_NOT_EXIST = -2
 
 
 
+const AdderRemover = (props) => (
+    <div>
+        { props.model.getNamesIds()
+        .filter(([name, id]) => props.selector(id))
+        .map(([name, id]) =>
+            <span key={id}>
+                <Label bsStyle="danger" onClick={() => props.changer(id, false)}>x</Label>
+                <Label bsStyle="success">{name}</Label>
+                <span> </span>
+            </span>)}
+        <DropdownButton id={props.title} bsSize="xsmall" title={props.title}>
+            {props.model.getNamesIds()
+                .filter(([name, id]) => !props.selector(id))
+                .map(([name, id]) => <MenuItem key={id} eventKey={id} onSelect={() => props.changer(id, true)}>{name}</MenuItem>)
+            }
+        </DropdownButton>
+    </div>
+)
+
 class ShowGameState extends React.Component {
+    constructor() {
+        super()
+        this.changeFlag = this.changeFlag.bind(this)
+    }
+
+    saveState = () => {
+        const {location, items, flags, variables} = this.props.state
+        const blob = new Blob(
+            [JSON.stringify({location, items, flags, variables})],
+            { type: 'text/plain' })
+        const anchor = document.createElement('a');
+        anchor.download = "stanje-igre.json";
+        anchor.href = (window.webkitURL || window.URL).createObjectURL(blob)
+        anchor.dataset.downloadurl = ['text/plain', anchor.download, anchor.href].join(':')
+        anchor.click()
+    }
+
+    loadState = files => {
+        const reader = new FileReader()
+        reader.onload = e => this.props.setGameState(JSON.parse(e.target.result))
+        reader.readAsText(files[0])
+    }
+
+    setLocation = location => this.props.setGameState({location})
+
+    changeFlag = (flag, state) => {
+        const flags = this.props.state.flags
+        flags[flag] = state
+        this.props.setGameState({flags})
+    }
+
+    takeDropItem = (item, state) => {
+        const items = this.props.state.items
+        items[item] = state ? ITEM_CARRIED : ITEM_DOES_NOT_EXIST
+        this.props.setGameState({items})
+    }
+
+    setVariable = (id, value) => {
+        const variables = this.props.state.variables
+        variables[id] = value
+        this.props.setGameState({variables})
+    }
+
     render() {
         const state = this.props.state
 
@@ -17,38 +79,65 @@ class ShowGameState extends React.Component {
             <Modal.Header closeButton>
                 <Modal.Title>
                     Stanje igre
+                    <Label bsStyle="success" onClick={this.saveState}>Shrani</Label>
+                    <FormControl id="stateUpload" style={{display: "none"}} type="file" accept=".json"
+                                 onChange={e => this.loadState(e.target.files)}/>
+                    <ControlLabel htmlFor="stateUpload">
+                        <Label bsStyle="success">Naloži</Label>
+                    </ControlLabel>
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <dl className="dl-horizontal">
                     <dt>Trenutna lokacija:</dt>
-                    <dd>{locations.getLocation(state.location).title}</dd>
+                    <dd>
+                        <DropdownButton id="current-location"
+                                        bsSize="xsmall"
+                                        title={locations.getLocation(state.location).title}>
+                            { locations.getIds().map(it =>
+                                <MenuItem key={it} eventKey={it} onSelect={this.setLocation}>
+                                    {locations.getLocation(it).title}</MenuItem>
+                            )}
+                        </DropdownButton>
+                    </dd>
+
 
                     <dt>Zastavice:</dt>
-                    <dd>{[...state.flags.values()]
-                        .map(it => flags.getNameById(it))
-                        .join(", ")
-                        || "(ni zastavic)"}</dd>
+                    <dd><AdderRemover title="Postavi zastavico"
+                                      model={flags}
+                                      selector={id => state.flags[id]}
+                                      changer={this.changeFlag} /></dd>
 
                     <dt>Igralec ima:</dt>
-                    <dd>{[...Object.entries(state.items)]
-                        .filter(it => it[1] == ITEM_CARRIED)
-                        .map(it => items.getNameById(it[0]))
-                        .join(", ")
-                        || "(nič)"}</dd>
-
-                    <dt>Ostale stvari:</dt>
-                    <dd>{[...Object.entries(state.items)]
-                        .filter(it => (it[1] != ITEM_DOES_NOT_EXIST) && (it[1] != ITEM_CARRIED))
-                        .map(it => `${items.getNameById(it[0])} -> ${locations.getLocation(it[1]).title}`)
-                        .join(", ")
-                        || "(ničesar ni)"}</dd>
+                    <dd><AdderRemover title="Vzemi stvar"
+                                      model={items}
+                                      selector={id => state.items[id] == ITEM_CARRIED}
+                                      changer={this.takeDropItem}/></dd>
 
                     <dt>Spremenljivke:</dt>
                     <dd>{[...Object.entries(state.variables)]
-                        .map(it => `${variables.getNameById(it[0])}=${it[1]}`)
-                        .join(", ")
-                        || "(ni spremenljivk)"}</dd>
+                        .map(([id, value]) => <span key={id}>
+                                {variables.getNameById(id)}&nbsp;=&nbsp;
+                            <span
+                                onKeyDown = {
+                                    e => {
+                                        if (e.keyCode == 13) {
+                                            const target = e.target
+                                            if (target.innerText == "")
+                                                target.innerText = "0"
+                                            this.setVariable(id, parseInt(target.innerText))
+                                            target.blur()
+                                        }
+                                        if ("0123456789\x08\x25\x27\x2e".indexOf(String.fromCharCode(e.keyCode)) == -1) {
+                                            e.preventDefault()
+                                        }
+                                    }
+                                }
+                                ref={node => { if (node) node.setAttribute("contentEditable", true)} }>
+                                {value}
+                            </span>
+                        </span>)
+                    }</dd>
                 </dl>
             </Modal.Body>
         </Modal>
@@ -63,7 +152,7 @@ export default class Game extends React.Component {
             location: locations.startLocation,
             printed: [],
             items: this._obj_from_keys(items.getIds(), ITEM_DOES_NOT_EXIST),
-            flags: new Set(),
+            flags: this._obj_from_keys(flags.getIds()),
             variables: this._obj_from_keys(variables.getIds()),
             showState: false
         }
@@ -125,8 +214,8 @@ export default class Game extends React.Component {
                 case 'item_exists': return this.state.items[condition.item] != ITEM_DOES_NOT_EXIST
                 case 'item_is_here': return this.state.items[condition.item] == this.state.location
 
-                case 'flag_set': return this.state.flags.has(condition.flag)
-                case 'flag_clear': return !this.state.flags.has(condition.flag)
+                case 'flag_set': return this.state.flags[condition.flag]
+                case 'flag_clear': return !this.state.flags[condition.flag]
 
                 case 'compare_const': return comp(condition.operator, this.state.variables[condition.variable], parseInt(condition.constant))
                 case 'compare_var': return comp(condition.operator, this.state.variables[condition.variable], this.state.variables[condition.variable2])
@@ -174,8 +263,8 @@ export default class Game extends React.Component {
             case 'item_at': this.state.items[block.item] = block.location; break
             case 'destroy': this.state.items[block.item] = ITEM_DOES_NOT_EXIST; break
 
-            case 'set_flag': this.state.flags.add(block.flag); break
-            case 'clear_flag': this.state.flags.delete(block.flag); break
+            case 'set_flag': this.state.flags[block.flag] = true; break
+            case 'clear_flag': this.state.flags[block.flag] = false; break
 
             case 'set_const': this.state.variables[block.variable] = parseInt(block.constant); break
             case 'increase': this.state.variables[block.variable]++; break
@@ -189,8 +278,8 @@ export default class Game extends React.Component {
     }
 
     showGameState= () => this.setState({showState: true})
-
     hideGameState = () => this.setState({showState: false})
+    setGameState = state => this.setState(state)
 
     render() {
         const dirmap = {"S": "n", "SV": "ne", "V": "e",
@@ -200,7 +289,7 @@ export default class Game extends React.Component {
         return (
             <Panel>
                 {this.state.showState
-                    ? <ShowGameState state={this.state} handleClose={this.hideGameState}/>
+                    ? <ShowGameState state={this.state} setGameState={this.setGameState} handleClose={this.hideGameState}/>
                     : ""
                 }
                 <Media>
@@ -236,4 +325,3 @@ export default class Game extends React.Component {
 }
 
 // TODO: Compass
-// TODO: Fix variables
